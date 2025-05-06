0a7445417408bfd27b4c4c0899fbb50cefb39b4d
Arnav495 <arnieincyberland@gmail.com>
2025-05-06 16:37:10 -0700

    Hello, triangle!

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,40 @@
-// gcc example/c/gl_sdl2.c build/src/gl.c -Ibuild/include `sdl2-config --libs --cflags` -ldl
 #include <glad/gl.h>
 
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_opengl.h>
 
+#include <print>
+#include <string>
+#include <unordered_map>
+
 const GLuint WIDTH = 800, HEIGHT = 600;
+
+typedef unsigned int uint;
+
+uint compileShader(const std::string& shaderSrc, const uint shaderType) {
+	static std::unordered_map<uint, std::string> reverseGlEnum{
+	    // {GL_COMPUTE_SHADER, "GL_COMPUTE_SHADER"},
+	    {GL_VERTEX_SHADER,   "GL_VERTEX_SHADER"  },
+	    // {GL_TESS_CONTROL_SHADER, "GL_TESS_CONTROL_SHADER"},
+	    // {GL_TESS_EVALUATION_SHADER, "GL_TESS_EVALUATION_SHADER"},
+	    {GL_GEOMETRY_SHADER, "GL_GEOMETRY_SHADER"},
+	    {GL_FRAGMENT_SHADER, "GL_FRAGMENT_SHADER"},
+	};
+
+	auto asPtr = shaderSrc.c_str();
+	uint shader = glCreateShader(shaderType);
+	glShaderSource(shader, 1, &asPtr, nullptr);
+	glCompileShader(shader);
+	int success;
+	char infoLog[512];
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+	if (not success) {
+		glGetShaderInfoLog(shader, 512, NULL, infoLog);
+		throw std::runtime_error(std::format("ERROR: failed to compile {} shader: {}.",
+		                                     reverseGlEnum[shaderType], infoLog));
+	}
+	return shader;
+}
 
 int main(void) {
 	// code without checking for errors
@@ -20,9 +50,59 @@ int main(void) {
 
 	SDL_GLContext context = SDL_GL_CreateContext(window);
 
-	int version [[maybe_unused]] = gladLoadGL((GLADloadfunc)SDL_GL_GetProcAddress);
+	int version = gladLoadGL((GLADloadfunc)SDL_GL_GetProcAddress);
+	std::println("GLAD version: {}", version);
 
-	glViewport(0, 0, 800, 600);
+	glViewport(0, 0, WIDTH, HEIGHT);
+
+	const char vertexShaderSrc[] = {
+#embed "./shaders/vertexShader.vert.glsl"
+	    , 0};
+	const char fragmentShaderSrc[] = {
+#embed "./shaders/fragmentShader.frag.glsl"
+	    , 0};
+
+	uint vertexShader = compileShader(std::string(vertexShaderSrc), GL_VERTEX_SHADER);
+	uint fragmentShader = compileShader(std::string(fragmentShaderSrc), GL_FRAGMENT_SHADER);
+
+	uint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	int success;
+	char infoLog[512];
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (not success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		throw std::runtime_error(std::format("ERROR: failed to link shader program: {}.", infoLog));
+	}
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	constexpr float vertices[] = {
+	    // clang-format off
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.0f,  0.5f, 0.0f
+	    // clang-format on
+	};
+
+	uint vertBufObj;
+	uint vertAttribObj;
+	glGenVertexArrays(1, &vertAttribObj);
+	glGenBuffers(1, &vertBufObj);
+
+	glBindVertexArray(vertAttribObj);
+	glBindBuffer(GL_ARRAY_BUFFER, vertBufObj);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindVertexArray(0);
+
+	std::println("vbo: {}; vao: {}", vertBufObj, vertAttribObj);
 
 	int exit = 0;
 	while (not exit) {
@@ -39,12 +119,20 @@ int main(void) {
 			}
 		}
 
-		glClearColor(0.7f, 0.9f, 0.1f, 1.0f);
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(shaderProgram);
+		glBindVertexArray(vertAttribObj);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		SDL_GL_SwapWindow(window);
 		SDL_Delay(1);
 	}
+
+	glDeleteVertexArrays(1, &vertAttribObj);
+	glDeleteBuffers(1, &vertBufObj);
+	glDeleteProgram(shaderProgram);
 
 	SDL_GL_DestroyContext(context);
 	SDL_DestroyWindow(window);
