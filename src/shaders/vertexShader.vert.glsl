4610842d1ac8f435abb3a65f3c502ae13deebd18
Arnav495 <arnieincyberland@gmail.com>
2025-05-07 15:59:35 -0700

    Revert "Excersize: pass position to fragment shader."
    This reverts commit 679103a5a08a153209a69dcd8fba21e106f3c3a8.

--- src/shaders/vertexShader.vert.glsl
+++ src/shaders/vertexShader.vert.glsl
@@ -2,12 +2,10 @@
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
 
-out vec3 vertPos;
 out vec3 pxColor;
 
 void main() {
 	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
-	vertPos = aPos;
 	pxColor = aColor;
 }
 
