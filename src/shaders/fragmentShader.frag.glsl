679103a5a08a153209a69dcd8fba21e106f3c3a8
Arnav495 <arnieincyberland@gmail.com>
2025-05-07 15:57:40 -0700

    Excersize: pass position to fragment shader.

--- src/shaders/fragmentShader.frag.glsl
+++ src/shaders/fragmentShader.frag.glsl
@@ -1,10 +1,17 @@
 #version 330 core
 in vec3 pxColor;
+in vec3 vertPos;
 
 out vec4 fragColor;
 
 uniform float greenColor;
 
 void main() {
-	fragColor = vec4(pxColor.r, greenColor, pxColor.b, 1.0f);
+	// fragColor = vec4(pxColor.r, greenColor, pxColor.b, 1.0f);
+	fragColor = vec4(
+		vertPos.x / 2.0f + 0.5f,
+		vertPos.y / 2.0f + 0.5f,
+		vertPos.z / 2.0f + 0.5f,
+		1.0f
+	);
 } 
