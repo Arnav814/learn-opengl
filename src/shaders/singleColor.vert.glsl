2d50df0eb7f95d4d682c7cfbd9688d497a81591a
Arnav495 <arnieincyberland@gmail.com>
2025-05-26 15:43:25 -0700

    Borders with stencil buffer.

---
+++ src/shaders/singleColor.vert.glsl
@@ -0,0 +1,15 @@
+#version 330 core
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aNormal;
+layout (location = 2) in vec2 aTexCoord;
+
+out vec3 fragPos;
+
+uniform mat4 obj2world;
+uniform mat4 world2cam;
+uniform mat4 projection;
+
+void main() {
+	gl_Position = projection * world2cam * obj2world * vec4(aPos, 1.0);
+}
+
